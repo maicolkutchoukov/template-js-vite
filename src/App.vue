7fd2db601caa768d581be826ea82ac257df15cf3
maicolkutchoukov <maicol.j.95@gmail.com>
2024-01-17 13:23:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+/* 
+1) Importazione del componente
+2) Dichiarazione del componente all'interno dell'oggetto
+3) Utilizzo del componente
+*/
+// 1) Importazione
+import AppHeader from './components/AppHeader.vue'
+export default {
+    data() {
+        return { 
+            count: 0 
+        };
+    },
+    // 2) Dichiarazione dei componenti
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <!-- 3) Utilizzo del componente -->
+        <AppHeader/>
+        <h1>
+            Prova contatore
+        </h1>
+        <p>
+            {{ count }}
+        </p>
+        <button @click="count++">
+            Incrementa count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return { 
+            title : 'Ciao Ragazzi' 
+        };
+    },
+};
+</script>
+
+<template>
+    <header class="main-title">
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        padding: 20px;
+        background-color: black;
+    }
+</style>
